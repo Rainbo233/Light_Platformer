0ac45419042e808ad82663d9c4bc6045f1c9828a
Allen Hall <54783249+Rainbo233@users.noreply.github.com>
2023-07-12 21:48:27 -0700

    Flashlight and Advanced movment
    Added a flashlight and player movement. Will work on allowing the flashlight t orotate with mouse and flipping the character when moving left next.

--- Light_Platformer/Player/player.gd
+++ Light_Platformer/Player/player.gd
@@ -1,38 +1,73 @@
 extends CharacterBody2D
 
+@export var speed := 300.0
+@export var dash_speed := 100
+@export var jump_velocity := -400.0
+@export var double_jump_enabled := false
+@export var dash_enabled := false
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -600.0
+@onready var dash_timer := $Dash_Timer
 
+var jumps := 2
+var dash_off_cooldown := true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-
-
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		
+		if Input.is_action_just_pressed("jump"):
+			jump(delta)
+		
+	
+	if is_on_floor():
+		
+		jumps = 2
+		
+	
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump(delta)
+	
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		velocity.x = move_toward(velocity.x, 0, speed)
 	
-	#Reloads level
-	if Input.is_action_just_pressed("r"):
-		get_tree().reload_current_scene()
+	# Handle Dash.
+	if Input.is_action_just_pressed("dash") and dash_enabled:
+		
+		if dash_off_cooldown:
+			dash(direction)
+			
+	
+	move_and_slide()
+
+func jump(delta) -> void:
+	
+	jumps -= 1
+	if jumps >= 0:
+		velocity.y = jump_velocity
+	
+
+func dash(direction) -> void:
+	
+	dash_off_cooldown = false
+	position.x += dash_speed * direction
+	dash_timer.start()
+	
+	
 
 
+func _on_dash_timer_timeout():
+	
+	dash_off_cooldown = true
+	
